7eb017f1d3444193033a1962ce0353e8197efc17
sawanoboly <sawanoboriyu@higanworks.com>
2012-12-07 20:27:38 +0900

    forget include template...

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "sawanoboriyu@higanworks.com"
 license          "MIT"
 description      "Installs/Configures hwraid from hwraid..le-vert.net"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version          "0.1.3"
 depends          "apt"
 depends          "hwraid" # add self for foodcritic
