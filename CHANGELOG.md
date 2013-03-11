47696f12ed613f9ab5c45bea20a79cca811361e3
sawanoboly <sawanoboriyu@higanworks.com>
2013-03-11 10:44:22 +0900

    bump v0.2.0

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+ChangeLog
+----
+
+### v0.2.0
+
+- Changed recipe filename because typo. HT: @Soulou
+
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "sawanoboriyu@higanworks.com"
 license          "MIT"
 description      "Installs/Configures hwraid from hwraid..le-vert.net"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.4"
+version          "0.2.0"
 depends          "apt"
 depends          "hwraid" # add self for foodcritic
